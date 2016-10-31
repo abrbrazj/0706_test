dc9ae0444046c82548142233bc48a8056de693a2
abrbrazj <abrbrazj@163.com>
2016-10-31 14:56:52 +0800

    主函数

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "make_log.h"
+
+int main(int argc,char *argv[])
+{
+
+    return 0;
+}
